232b5cf10b2d05f273c9012e26ccfbb1e17e5e44
unknown <oscar.mnfuentes@gmail.com>
2025-04-29 19:41:55 +0200

    Restore apps url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ document.getElementById('rsvp-form').addEventListener('submit', async function (
 e.preventDefault();
 const form = e.target;
 const data = new FormData(form);
-const url = 'https://script.google.com/macros/library/d/1uPm02po4jPYzobsycjzYH1q7_cNBzcxKM5mgHMwWi3KkHv3QIifpKXQA/1';
+const url = 'https://script.google.com/macros/s/AKfycbzB-DTKISdLGZZDFdfMGkHjxyVac3FXNU6TkIXRFDfWcHzLO-WUlvAreSH3eUtk2pco/exec';
 try {
 const response = await fetch(url, { method: 'POST', body: data });
 if (response.ok) {
